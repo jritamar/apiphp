c7bded7a8d28b321d59007c736b8023d154daab6
Itamar Junior <jritamar@istecn.com.br>
2023-10-26 16:33:49 -0300

    Api PHP DIO

--- Config/config.php
+++ Config/config.php
@@ -3,4 +3,10 @@ define("ROOT_PATH",__DIR__ . "/../" );
 
 define("DATABASE_FILE", ROOT_PATH . 'database.json');
 
+require_once ROOT_PATH . "/Controller/Api/BaseController.php";
+
+require_once ROOT_PATH . "/Model/UserModel.php";
+
+
+
 ?>
