c027a330ab015f18989ee76b23122f6ab7b6a259
Itamar Junior <104800630+jritamar@users.noreply.github.com>
2023-11-13 16:56:37 -0300

    TAPI

---
+++ Controller/API/UserController.php
@@ -0,0 +1,9 @@
+<?php
+
+class UserController extends BaseController
+{
+    public function listAction(){
+        $erroDescription = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+    }
+}
